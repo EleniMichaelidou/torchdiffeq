ec905e7f995fd2e7ade9787206eed20fc89bfd89
Eleni Michaelidou <eleni.michaelidou@outlook.com>
2023-08-13 00:58:21 +0100

    cnf changes

--- examples/cnf.py
+++ examples/cnf.py
@@ -10,14 +10,15 @@ from sklearn.datasets import make_circles
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import pandas as pd
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--adjoint', action='store_true')
 parser.add_argument('--viz', action='store_true')
-parser.add_argument('--niters', type=int, default=1000)
+parser.add_argument('--niters', type=int, default=100)
 parser.add_argument('--lr', type=float, default=1e-3)
-parser.add_argument('--num_samples', type=int, default=512)
+parser.add_argument('--_num_samples', type=int, default=512)
 parser.add_argument('--width', type=int, default=64)
 parser.add_argument('--hidden_dim', type=int, default=32)
 parser.add_argument('--gpu', type=int, default=0)
@@ -42,16 +43,15 @@ class CNF(nn.Module):
         self.width = width
         self.hyper_net = HyperNetwork(in_out_dim, hidden_dim, width)
 
-    def forward(self, t, states):
+    def forward(self, _t, states):
         z = states[0]
-        logp_z = states[1]
 
         batchsize = z.shape[0]
 
         with torch.set_grad_enabled(True):
             z.requires_grad_(True)
 
-            W, B, U = self.hyper_net(t)
+            W, B, U = self.hyper_net(_t)
 
             Z = torch.unsqueeze(z, 0).repeat(self.width, 1, 1)
 
@@ -60,7 +60,7 @@ class CNF(nn.Module):
 
             dlogp_z_dt = -trace_df_dz(dz_dt, z).view(batchsize, 1)
 
-        return (dz_dt, dlogp_z_dt)
+        return dz_dt, dlogp_z_dt
 
 
 def trace_df_dz(f, z):
@@ -94,9 +94,9 @@ class HyperNetwork(nn.Module):
         self.width = width
         self.blocksize = blocksize
 
-    def forward(self, t):
+    def forward(self, _t):
         # predict params
-        params = t.reshape(1, 1)
+        params = _t.reshape(1, 1)
         params = torch.tanh(self.fc1(params))
         params = torch.tanh(self.fc2(params))
         params = self.fc3(params)
@@ -118,6 +118,8 @@ class RunningAverageMeter(object):
     """Computes and stores the average and current value"""
 
     def __init__(self, momentum=0.99):
+        self.avg = None
+        self.val = None
         self.momentum = momentum
         self.reset()
 
@@ -133,154 +135,197 @@ class RunningAverageMeter(object):
         self.val = val
 
 
-def get_batch(num_samples):
-    points, _ = make_circles(n_samples=num_samples, noise=0.06, factor=0.5)
-    x = torch.tensor(points).type(torch.float32).to(device)
-    logp_diff_t1 = torch.zeros(num_samples, 1).type(torch.float32).to(device)
+def get_batch(_num_samples, _noise_level):
+    _points, _ = make_circles(n_samples=_num_samples, noise=_noise_level, factor=0.5)
+    _x = torch.tensor(_points).type(torch.float32).to(device)
+    _logp_diff_t1 = torch.zeros(_num_samples, 1).type(torch.float32).to(device)
 
-    return(x, logp_diff_t1)
+    return _x, _logp_diff_t1
 
 
+import itertools
 if __name__ == '__main__':
+    # Define a list of noise levels and sample sizes you want to iterate over
+    noise_levels = [0, 0.01, 0.05, 0.1]  # Example noise levels
+    num_samples_list = [64, 128, 256, 512, 1024]  # Example sample sizes
+
     t0 = 0
     t1 = 10
-    device = torch.device('cuda:' + str(args.gpu)
-                          if torch.cuda.is_available() else 'cpu')
+    for noise_level, num_samples in itertools.product(noise_levels, num_samples_list):
+        device = torch.device(
+            f'cuda:{str(args.gpu)}' if torch.cuda.is_available() else 'cpu'
+        )
 
-    # model
-    func = CNF(in_out_dim=2, hidden_dim=args.hidden_dim, width=args.width).to(device)
-    optimizer = optim.Adam(func.parameters(), lr=args.lr)
-    p_z0 = torch.distributions.MultivariateNormal(
-        loc=torch.tensor([0.0, 0.0]).to(device),
-        covariance_matrix=torch.tensor([[0.1, 0.0], [0.0, 0.1]]).to(device)
-    )
-    loss_meter = RunningAverageMeter()
+        average_loss_list = []  # Add this line before the training loop
+        current_loss_list = []
 
-    if args.train_dir is not None:
-        if not os.path.exists(args.train_dir):
-            os.makedirs(args.train_dir)
-        ckpt_path = os.path.join(args.train_dir, 'ckpt.pth')
-        if os.path.exists(ckpt_path):
-            checkpoint = torch.load(ckpt_path)
-            func.load_state_dict(checkpoint['func_state_dict'])
-            optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-            print('Loaded ckpt from {}'.format(ckpt_path))
+        # model
+        func = CNF(in_out_dim=2, hidden_dim=args.hidden_dim, width=args.width).to(device)
+        optimizer = optim.Adam(func.parameters(), lr=args.lr)
+        p_z0 = torch.distributions.MultivariateNormal(
+            loc=torch.tensor([0.0, 0.0]).to(device),
+            covariance_matrix=torch.tensor([[0.1, 0.0], [0.0, 0.1]]).to(device)
+        )
+        loss_meter = RunningAverageMeter()
 
-    try:
-        for itr in range(1, args.niters + 1):
-            optimizer.zero_grad()
-
-            x, logp_diff_t1 = get_batch(args.num_samples)
-
-            z_t, logp_diff_t = odeint(
-                func,
-                (x, logp_diff_t1),
-                torch.tensor([t1, t0]).type(torch.float32).to(device),
-                atol=1e-5,
-                rtol=1e-5,
-                method='dopri5',
-            )
-
-            z_t0, logp_diff_t0 = z_t[-1], logp_diff_t[-1]
-
-            logp_x = p_z0.log_prob(z_t0).to(device) - logp_diff_t0.view(-1)
-            loss = -logp_x.mean(0)
-
-            loss.backward()
-            optimizer.step()
-
-            loss_meter.update(loss.item())
-
-            print('Iter: {}, running avg loss: {:.4f}'.format(itr, loss_meter.avg))
-
-    except KeyboardInterrupt:
         if args.train_dir is not None:
+            if not os.path.exists(args.train_dir):
+                os.makedirs(args.train_dir)
             ckpt_path = os.path.join(args.train_dir, 'ckpt.pth')
-            torch.save({
-                'func_state_dict': func.state_dict(),
-                'optimizer_state_dict': optimizer.state_dict(),
-            }, ckpt_path)
-            print('Stored ckpt at {}'.format(ckpt_path))
-    print('Training complete after {} iters.'.format(itr))
+            if os.path.exists(ckpt_path):
+                checkpoint = torch.load(ckpt_path)
+                func.load_state_dict(checkpoint['func_state_dict'])
+                optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+                print(f'Loaded ckpt from {ckpt_path}')
 
-    if args.viz:
-        viz_samples = 30000
-        viz_timesteps = 41
-        target_sample, _ = get_batch(viz_samples)
+        try:
+            for itr in range(1, args.niters + 1):
+                optimizer.zero_grad()
 
-        if not os.path.exists(args.results_dir):
-            os.makedirs(args.results_dir)
-        with torch.no_grad():
-            # Generate evolution of samples
-            z_t0 = p_z0.sample([viz_samples]).to(device)
-            logp_diff_t0 = torch.zeros(viz_samples, 1).type(torch.float32).to(device)
+                x, logp_diff_t1 = get_batch(num_samples, noise_level)
 
-            z_t_samples, _ = odeint(
-                func,
-                (z_t0, logp_diff_t0),
-                torch.tensor(np.linspace(t0, t1, viz_timesteps)).to(device),
-                atol=1e-5,
-                rtol=1e-5,
-                method='dopri5',
+                z_t, logp_diff_t = odeint(
+                    func,
+                    (x, logp_diff_t1),
+                    torch.tensor([t1, t0]).type(torch.float32).to(device),
+                    atol=1e-5,
+                    rtol=1e-5,
+                    method='dopri5',
+                )
+
+                z_t0, logp_diff_t0 = z_t[-1], logp_diff_t[-1]
+
+                logp_x = p_z0.log_prob(z_t0).to(device) - logp_diff_t0.view(-1)
+                loss = -logp_x.mean(0)
+
+                loss.backward()
+                optimizer.step()
+
+                loss_meter.update(loss.item())
+                average_loss_list.append(loss_meter.avg)  # Add this line to store the loss value
+                current_loss_list.append(loss.item())
+
+                print('Iter: {}, running avg loss: {:.4f}'.format(itr, loss_meter.avg))
+
+        except KeyboardInterrupt:
+            if args.train_dir is not None:
+                ckpt_path = os.path.join(args.train_dir, 'ckpt.pth')
+                torch.save({
+                    'func_state_dict': func.state_dict(),
+                    'optimizer_state_dict': optimizer.state_dict(),
+                }, ckpt_path)
+                print(f'Stored ckpt at {ckpt_path}')
+        print(f'Training complete after {itr} iters.')
+
+        if args.viz:
+            viz_samples = num_samples
+            viz_timesteps = 41
+            target_sample, _ = get_batch(viz_samples, noise_level)
+
+            # Create a unique results directory for each run
+            results_dir = f"./results_{noise_level}_{num_samples}"
+            if not os.path.exists(results_dir):
+                os.makedirs(results_dir)
+            with torch.no_grad():
+                # Generate evolution of samples
+                z_t0 = p_z0.sample([viz_samples]).to(device)
+                logp_diff_t0 = torch.zeros(viz_samples, 1).type(torch.float32).to(device)
+
+                z_t_samples, _ = odeint(
+                    func,
+                    (z_t0, logp_diff_t0),
+                    torch.tensor(np.linspace(t0, t1, viz_timesteps)).to(device),
+                    atol=1e-5,
+                    rtol=1e-5,
+                    method='dopri5',
+                )
+
+                # Generate evolution of density
+                x = np.linspace(-1.5, 1.5, 100)
+                y = np.linspace(-1.5, 1.5, 100)
+                points = np.vstack(np.meshgrid(x, y)).reshape([2, -1]).T
+
+                z_t1 = torch.tensor(points).type(torch.float32).to(device)
+                logp_diff_t1 = torch.zeros(z_t1.shape[0], 1).type(torch.float32).to(device)
+
+                z_t_density, logp_diff_t = odeint(
+                    func,
+                    (z_t1, logp_diff_t1),
+                    torch.tensor(np.linspace(t1, t0, viz_timesteps)).to(device),
+                    atol=1e-5,
+                    rtol=1e-5,
+                    method='dopri5',
+                )
+
+                # Create plots for each timestep
+                for (t, z_sample, z_density, logp_diff) in zip(
+                        np.linspace(t0, t1, viz_timesteps),
+                        z_t_samples, z_t_density, logp_diff_t
+                ):
+                    fig = plt.figure(figsize=(12, 4), dpi=200)
+                    plt.tight_layout()
+                    plt.axis('off')
+                    plt.margins(0, 0)
+                    fig.suptitle(f'{t:.2f}s')
+
+                    ax1 = fig.add_subplot(1, 3, 1)
+                    ax1.set_title('Target')
+                    ax1.get_xaxis().set_ticks([])
+                    ax1.get_yaxis().set_ticks([])
+                    ax2 = fig.add_subplot(1, 3, 2)
+                    ax2.set_title('Samples')
+                    ax2.get_xaxis().set_ticks([])
+                    ax2.get_yaxis().set_ticks([])
+                    ax3 = fig.add_subplot(1, 3, 3)
+                    ax3.set_title('Log Probability')
+                    ax3.get_xaxis().set_ticks([])
+                    ax3.get_yaxis().set_ticks([])
+
+                    ax1.hist2d(*target_sample.detach().cpu().numpy().T, bins=300, density=True,
+                               range=[[-1.5, 1.5], [-1.5, 1.5]])
+
+                    ax2.hist2d(*z_sample.detach().cpu().numpy().T, bins=300, density=True,
+                               range=[[-1.5, 1.5], [-1.5, 1.5]])
+
+                    logp = p_z0.log_prob(z_density) - logp_diff.view(-1)
+                    ax3.tricontourf(*z_t1.detach().cpu().numpy().T,
+                                    np.exp(logp.detach().cpu().numpy()), 200)
+
+                    plt.savefig(os.path.join(results_dir, f"cnf-viz-{int(t*1000):05d}.jpg"),
+                               pad_inches=0.2, bbox_inches='tight')
+                    plt.close()
+
+                img, *imgs = [
+                    Image.open(f)
+                    for f in sorted(
+                        glob.glob(os.path.join(results_dir, "cnf-viz-*.jpg"))
+                    )
+                ]
+                img.save(fp=os.path.join(results_dir, "cnf-viz.gif"), format='GIF', append_images=imgs,
+                         save_all=True, duration=250, loop=0)
+
+            print(
+                f'Saved visualization animation at {os.path.join(results_dir, "cnf-viz.gif")}'
             )
 
-            # Generate evolution of density
-            x = np.linspace(-1.5, 1.5, 100)
-            y = np.linspace(-1.5, 1.5, 100)
-            points = np.vstack(np.meshgrid(x, y)).reshape([2, -1]).T
+            loss_df = pd.DataFrame({'Average Loss': average_loss_list, 'Current Loss': current_loss_list})
+            excel_filename = os.path.join(results_dir, f'loss_data_{noise_level}_{num_samples}.xlsx')
+            loss_df.to_excel(excel_filename, index=False)
 
-            z_t1 = torch.tensor(points).type(torch.float32).to(device)
-            logp_diff_t1 = torch.zeros(z_t1.shape[0], 1).type(torch.float32).to(device)
-
-            z_t_density, logp_diff_t = odeint(
-                func,
-                (z_t1, logp_diff_t1),
-                torch.tensor(np.linspace(t1, t0, viz_timesteps)).to(device),
-                atol=1e-5,
-                rtol=1e-5,
-                method='dopri5',
+            print(
+                f'Saved loss values at {os.path.join(results_dir, f"loss_data_{noise_level}_{num_samples}.xlsx")}'
             )
 
-            # Create plots for each timestep
-            for (t, z_sample, z_density, logp_diff) in zip(
-                    np.linspace(t0, t1, viz_timesteps),
-                    z_t_samples, z_t_density, logp_diff_t
-            ):
-                fig = plt.figure(figsize=(12, 4), dpi=200)
-                plt.tight_layout()
-                plt.axis('off')
-                plt.margins(0, 0)
-                fig.suptitle(f'{t:.2f}s')
+            # After the training loop
+            plt.figure()
+            plt.plot(average_loss_list, label='Average Loss')
+            plt.plot(current_loss_list, label='Current Loss', alpha=0.5)
+            plt.xlabel('Iteration')
+            plt.ylabel('Loss')
+            plt.title(f'Loss During Training (Noise Level: {noise_level}, Num Samples: {num_samples})')
+            plt.legend()
+            plt.savefig(os.path.join(results_dir, f'loss_plot_{noise_level}_{num_samples}.png'))
 
-                ax1 = fig.add_subplot(1, 3, 1)
-                ax1.set_title('Target')
-                ax1.get_xaxis().set_ticks([])
-                ax1.get_yaxis().set_ticks([])
-                ax2 = fig.add_subplot(1, 3, 2)
-                ax2.set_title('Samples')
-                ax2.get_xaxis().set_ticks([])
-                ax2.get_yaxis().set_ticks([])
-                ax3 = fig.add_subplot(1, 3, 3)
-                ax3.set_title('Log Probability')
-                ax3.get_xaxis().set_ticks([])
-                ax3.get_yaxis().set_ticks([])
-
-                ax1.hist2d(*target_sample.detach().cpu().numpy().T, bins=300, density=True,
-                           range=[[-1.5, 1.5], [-1.5, 1.5]])
-
-                ax2.hist2d(*z_sample.detach().cpu().numpy().T, bins=300, density=True,
-                           range=[[-1.5, 1.5], [-1.5, 1.5]])
-
-                logp = p_z0.log_prob(z_density) - logp_diff.view(-1)
-                ax3.tricontourf(*z_t1.detach().cpu().numpy().T,
-                                np.exp(logp.detach().cpu().numpy()), 200)
-
-                plt.savefig(os.path.join(args.results_dir, f"cnf-viz-{int(t*1000):05d}.jpg"),
-                           pad_inches=0.2, bbox_inches='tight')
-                plt.close()
-
-            img, *imgs = [Image.open(f) for f in sorted(glob.glob(os.path.join(args.results_dir, f"cnf-viz-*.jpg")))]
-            img.save(fp=os.path.join(args.results_dir, "cnf-viz.gif"), format='GIF', append_images=imgs,
-                     save_all=True, duration=250, loop=0)
-
-        print('Saved visualization animation at {}'.format(os.path.join(args.results_dir, "cnf-viz.gif")))
+            print(
+                f'Saved loss plot at {os.path.join(results_dir, f"loss_plot_{noise_level}_{num_samples}.png")}'
+            )
